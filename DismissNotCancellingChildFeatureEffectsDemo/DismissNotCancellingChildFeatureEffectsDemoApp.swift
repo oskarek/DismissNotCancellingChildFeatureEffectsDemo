df7f50d3b9a9949a9abdd61a3a7b7506adcc07b4
Oskar Ek <oskar.ek@me.com>
2024-04-16 16:35:46 +0200

    Initial Commit

---
+++ DismissNotCancellingChildFeatureEffectsDemo/DismissNotCancellingChildFeatureEffectsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DismissNotCancellingChildFeatureEffectsDemoApp.swift
+//  DismissNotCancellingChildFeatureEffectsDemo
+//
+//  Created by Oskar Ek on 2024-04-16.
+//
+
+import SwiftUI
+
+@main
+struct DismissNotCancellingChildFeatureEffectsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
